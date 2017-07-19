de1b036df4277af0360f825833e3b07cfb2eaaa2
Jack Burdick <jackbburdick@gmail.com>
2017-07-18 22:20:57 -0400

    View image stats

---
+++ docker_client/testing_playground/view_image_stats.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, image := range images {
+		if strings.Join(image.RepoTags, "") == "jackburdick/cosineexp:latest" {
+			fmt.Printf("Command: %v\n", image.Containers)
+			fmt.Printf("Created: %v\n", image.Created)
+			fmt.Printf("ID: %v\n", image.ID)
+			fmt.Printf("Labels: %v\n", image.Labels)
+			fmt.Printf("ParentID: %v\n", image.ParentID)
+			fmt.Printf("RepoDigests: %v\n", image.RepoDigests)
+			fmt.Printf("RepoTags: %v\n", image.RepoTags)
+			fmt.Printf("SharedSize: %v\n", image.SharedSize)
+			fmt.Printf("Size: %v\n", image.Size)
+			fmt.Printf("VirtualSize: %v\n", image.VirtualSize)
+			fmt.Printf("----------------------------------------\n")
+		}
+	}
+}
+
+// func (cli *Client) ContainerStart(ctx context.Context, containerID string, options types.ContainerStartOptions) error
