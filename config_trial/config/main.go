a6e62c7d3894bc6fde9a00519574ef70fac23311
Jack Burdick <jackbburdick@gmail.com>
2017-07-24 16:25:56 -0700

    Include dd_container specific example

--- config_trial/config/main.go
+++ config_trial/config/main.go
@@ -10,7 +10,7 @@ import (
 // read YAML
 
 func main() {
-	viper.SetConfigFile("./example_config.yml")
+	viper.SetConfigFile("./dd_example.yml")
 	// Searches for config file in given paths and read it
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalf("Error reading config file, %s", err)
@@ -18,4 +18,34 @@ func main() {
 
 	// Confirm which config file is used
 	fmt.Printf("Using config: %s\n", viper.ConfigFileUsed())
+
+	pathToDockerfile := viper.Get("pathToDockerfile")
+	fmt.Printf("pathToDockerfile: %v\n", pathToDockerfile)
+
+	endPointName := viper.Get("endPointName")
+	fmt.Printf("endPointName: %v\n", endPointName)
+
+	inputPath := viper.Get("inputPath")
+	fmt.Printf("inputPath: %v\n", inputPath)
+
+	hostIP := viper.Get("hostIP")
+	fmt.Printf("hostIP: %v\n", hostIP)
+
+	hostPort := viper.Get("hostPort")
+	fmt.Printf("hostPort: %v\n", hostPort)
+
+	containerName := viper.Get("containerName")
+	fmt.Printf("containerName: %v\n", containerName)
+
+	userName := viper.Get("userName")
+	fmt.Printf("userName: %v\n", userName)
+
+	imgName := viper.Get("imgName")
+	fmt.Printf("imgName: %v\n", imgName)
+
+	uN := fmt.Sprintf("%v", userName)
+	iN := fmt.Sprintf("%v", imgName)
+
+	imgHandle := uN + "/" + iN
+	fmt.Printf("imgHandle: %v\n", imgHandle)
 }
