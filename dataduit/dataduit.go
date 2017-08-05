b9743d77082d0e8a08c1ab4268b2bbbdf99546ac
Jack Burdick <jackbburdick@gmail.com>
2017-08-04 22:45:20 -0400

    Preprocessing functions moved back to main

--- dataduit/dataduit.go
+++ dataduit/dataduit.go
@@ -3,7 +3,6 @@ package dataduit
 import (
 	"bytes"
 	"compress/gzip"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -18,7 +17,6 @@ import (
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
 	"github.com/docker/go-connections/nat"
-	"github.com/jdkato/prose/tokenize"
 	"github.com/jhoonb/archivex"
 	"github.com/spf13/viper"
 )
@@ -162,36 +160,8 @@ func (dd *DdContainer) startDD() error {
 
 // useDD is a wrapper function that uses the container by passing input to the
 // main functionality and returning the result.
-func (dd *DdContainer) useDD(inputPath string, preProcess string) (string, error) {
+func (dd *DdContainer) useDD(jsonData []byte) (string, error) {
 	var result string
-
-	// Create map from input directory.
-	// TODO: handle input information (file vs dir)
-	// TODO: handle input preprocessing function.
-	var jsonData []byte
-	if preProcess == "cosine" {
-		fileMap, err := createMap(inputPath)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		// Build json data from map.
-		jsonData, err = json.Marshal(fileMap)
-		if err != nil {
-			fmt.Println(err)
-		}
-	} else if preProcess == "sudoku" {
-		fileMap, err := createSudokuInput(inputPath)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		// Build json data from map.
-		jsonData, err = json.Marshal(fileMap)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
 
 	// Gzip (lossless compression) json data.
 	var buf bytes.Buffer
@@ -269,13 +239,13 @@ func (dd *DdContainer) endDD() error {
 // CompleteDD is a wrapper function that wraps the above Build, startDD,
 // useDD, and endDD into one function call. This function is useful if the
 // container is only going to be used once.
-func (dd *DdContainer) CompleteDD(inputPath string, preProcessMethod string) (string, error) {
+func (dd *DdContainer) CompleteDD(jsonData []byte) (string, error) {
 	err := dd.startDD()
 	if err != nil {
 		fmt.Printf("Error starting container: %v\n", err)
 	}
 
-	res, err := dd.useDD(inputPath, preProcessMethod)
+	res, err := dd.useDD(jsonData)
 	if err != nil {
 		fmt.Printf("Error using container: %v\n", err)
 	}
@@ -407,90 +377,3 @@ func deleteImageByTag(cntx context.Context, imgTag string, images []types.ImageS
 		}
 	}
 }
-
-// ------------------ HELPER FUNCTIONS USED BY THE ABOVE WRAPPER FUNCTIONS [END]
-
-// ---------------------------------------------- INPUT PREPROCESSING FUNCTIONS
-// NOTE: This will likely fall on the user to create the specified input data.
-// and will only be included as an example that the user can pull from.
-
-// createMap is a helper that accepts a path to a directory and creates the
-// input data for the model.
-
-func createMap(dPath string) (map[string][]string, error) {
-	fileMap := make(map[string][]string)
-
-	// Create map of filename to tokenized content.
-	dFiles, _ := ioutil.ReadDir(dPath)
-	for _, f := range dFiles {
-		b, err := ioutil.ReadFile(dPath + f.Name())
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		// Convert bytes to string, then use 3rd party to tokenize.
-		fileMap[f.Name()] = tokenize.TextToWords(string(b))
-	}
-
-	return fileMap, nil
-}
-
-// ------------------------------------------------[END] cosine
-
-// ------------------------------------------------ Sudoku
-// crossIndex 'crosses' two strings such that the two individual values from
-// each string join together to create a new value.  For example, if string one
-// is "ABC" and string two is "123", the resulting return value will be;
-// ["A1","A2","A3","B1","B2","B3","C1","C2","C3"].
-func crossIndex(A string, N string) []string {
-	var ks []string
-	for _, a := range A {
-		for _, n := range N {
-			ks = append(ks, (string(a) + string(n)))
-		}
-	}
-	return ks
-}
-
-func createSudokuInput(fPath string) (map[string][]string, error) {
-	sudokuMap := make(map[string][]string)
-
-	data, err := ioutil.ReadFile(fPath)
-	if err != nil {
-		return sudokuMap, err
-	}
-
-	// Global board information.  The Sudoku board is assumed to be a standard
-	// 9x9 (A-I)x(1-9) grid -- where the first index (upper left) would be `A1`
-	// and the last index (lower right) would be `I9`.
-	rows := "ABCDEFGHI"
-	cols := "123456789"
-	inds := crossIndex(rows, cols)
-
-	// Convert the string representing the board into a grid(map) that maps a
-	// key (index) to the values (label for the box, or possible label for the
-	// box). for instance, if we know A1=7, map['A1'] = '7', but if the given
-	// index is empty (B2, as an example), the corresponding value would be
-	// '123456789' (map['B2'] = '123456789') NOTE: i acts as an increment for
-	// every target character found.
-	i := 0
-	for _, c := range data {
-		switch string(c) {
-		case "_":
-			sudokuMap[inds[i]] = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
-			i++
-		case "1", "2", "3", "4", "5", "6", "7", "8", "9":
-			sudokuMap[inds[i]] = []string{string(c)}
-			i++
-		case "\n", " ", "\r":
-			continue
-		default:
-			return sudokuMap, fmt.Errorf("unexpected value (%v) in Sudoku input", c)
-		}
-	}
-
-	return sudokuMap, nil
-}
-
-//------------------------------------------------ Sudoku [END]
-// ----------------------------------------- INPUT PREPROCESSING FUNCTIONS [END]
