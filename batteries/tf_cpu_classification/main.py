f5fd72b4f54c06c6fd02e755d5fc56fb11a15555
Jack Burdick <jackbburdick@gmail.com>
2017-10-27 12:05:27 -0400

    access headers, need to access body

--- batteries/tf_cpu_classification/main.py
+++ batteries/tf_cpu_classification/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 import http.server
 import socketserver
 
@@ -7,10 +8,13 @@ class HttpHandler(http.server.SimpleHTTPRequestHandler):
 
     def do_GET(self):
         if self.path == "/tf_cpu_classification":
+            r_headers = str(self.headers)
+            #data_string = self.rfile.read(int(self.headers['Content-Length']))
+
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
-            self.wfile.write(bytes("<html> <head><title> Testing </title> </head><body><p> some sweet classification will happen here.... </p>"+str(self.headers)+"</body>", "UTF-8"))
+            self.wfile.write(bytes("<html> <head><title> Testing </title> </head><body><p> some sweet classification will happen here.... </p>"+r_headers+"</body>", "UTF-8"))
         else:
             http.server.SimpleHTTPRequestHandler.do_GET(self)
 
